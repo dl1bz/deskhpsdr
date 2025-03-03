1583bb61c6a4c9a490bda45ce930cf0b9ad4f3cd
DL1BZ <dl1bz@bzsax.de>
2025-03-03 15:13:28 +0100

    remove commit c27f3cd: quick'n'dirty S-Meter calibration if SDR is Hermes Lite 2

--- src/rx_panadapter.c
+++ src/rx_panadapter.c
@@ -764,9 +764,11 @@ void rx_panadapter_update(RECEIVER *rx) {
       if (peak_positions[j] > 0) {
         char peak_label[32];
 #if defined (__LDESK__)
+#if defined (__USELESS__)
         if (device == DEVICE_HERMES_LITE2) {
           peaks[j] += 6.0;
         }
+#endif
         if (active_receiver->panadapter_peaks_as_smeter) {
           snprintf(peak_label, sizeof(peak_label), "%s", dbm2smeter[get_SWert((int)(peaks[j]))]);
         } else {
