a2e73977e32f03b532405f3f3c2740bbbe91a0ae
DL1BZ <dl1bz@bzsax.de>
2025-06-23 13:36:54 +0200

    cleanup some unused code for show compiler options at startup

--- src/version.c
+++ src/version.c
@@ -52,12 +52,9 @@ char build_options[] =
 #ifdef EXTNR
   "EXTNR "
 #endif
-#ifdef CLIENT_SERVER
-  "SERVER "
-#endif
-#ifdef __LDESK__
-  "DESKTOP "
-#endif
+// #ifdef __LDESK__
+//  "DESKTOP "
+// #endif
 #ifdef __HAVEATU__
   "ATU "
 #endif
