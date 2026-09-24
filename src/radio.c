c41204689ccc561ae1c4d902017b6e5175694674
dl1bz <dl1bz@bzsax.de>
2026-09-24 19:24:39 +0200

    Correct detetction mismatch Brick 2 <-> Brick 3

--- src/radio.c
+++ src/radio.c
@@ -2836,7 +2836,11 @@ static void radio_restore_state(void) {
   }
   GetPropI0("hl2_cl1_input",                                 hl2_cl1_input)
   GetPropI0("anan10E",                                       anan10E);
-  GetPropI0("hermes_mode",                                   hermes_mode);
+  if (!(protocol == NEW_PROTOCOL && radio != NULL &&
+    radio->info.network.mac_address[0] == 0x02 &&
+    radio->info.network.mac_address[1] == 0xB3)) {
+    GetPropI0("hermes_mode",                                 hermes_mode);
+  }
   GetPropI0("tci_audio_monitor",                             tci_audio_monitor);
   GetPropI0("tci_iq_swap",                                   tci_iq_swap);
   GetPropI0("tci_iq_conjugate",                              tci_iq_conjugate);
@@ -2847,8 +2851,7 @@ static void radio_restore_state(void) {
   GetPropI0("filter_board",                                  filter_board);
   if (protocol == NEW_PROTOCOL && radio != NULL &&
       radio->info.network.mac_address[0] == 0x02 &&
-      (radio->info.network.mac_address[1] == 0xB2 ||
-       radio->info.network.mac_address[1] == 0xB3)) {
+      radio->info.network.mac_address[1] == 0xB2) {
     hermes_mode = HERMES_MODE_BRICK;
     filter_board = ALEX;
   }
